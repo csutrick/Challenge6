ea7c10f2aa7b5c3ed14b5c2561e6a22888fa2198
Cole Sutrick <papacole@Coles-MacBook-Pro-4.local>
2023-07-17 12:41:43 -0500

    Fixed bugs and redid append function

--- Develop/script.js
+++ Develop/script.js
@@ -1,60 +1,159 @@
 var searchButton = document.getElementById('search-button');
 var cityInput = document.getElementById('userInput');
-var fiveDayContainer = document.getElementById('fiveDay-content');
+var pastSearchContainer = document.getElementById('container-pastSearch')
 var cityInputTerm = document.getElementById('header-locationDate');
+var currentDayContainer = document.getElementById('container-currentTemp');
+var fiveDayContainer = document.getElementById('fiveDay-content');
 
+searchTerms = [];
+
+// Handles the user search
 var inputHandler = function (event) {
     event.preventDefault();
 
-    var city = cityInput.value.trim()
+    var city = cityInput.value.trim();
 
     if (city) {
         getApi(city);
 
-        cityInput.value = '';
-        fiveDayContainer.textContent = 'working';
         } else {
         alert('Please enter a valid city');
     }
 };
-
-
-
+// API function
 var getApi = function(city) {
+    // URL
     var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + ',usa&units=imperial&cnt=6&APPID=adf18ae524fd38390fa6667d35153b0c';
-
+    // Fetch
     fetch(requestUrl)
         .then(function (response) {
             if (response.ok) {
-            response.json().then(function (data) {
-            displayWeather(data, city);
-            });
+                console.log('city found')
+                response.json().then(function (data) {
+                    addStorage(city);
+                    displayWeather(data, city);
+                });
             } else {
-            alert('Error: ' + response.statusText);
+                console.log('not a city')
+                alert('Error: ' + response.statusText);
             }
         })
         .catch(function (error) {
             alert('Unable to connect to weather service');
         });
 };
-
-
+// Appending content to the page
 var displayWeather = function(data, searchTerm) {
-    console.log('getting data')
-
-    cityInputTerm.textContent = searchTerm;
-
+    // Resets containers
+    currentDayContainer.innerHTML = '';
+    fiveDayContainer.innerHTML = '';
     for (var i = 0; i < data.list.length; i++) {
+        
+        // Converting the date/time
+        const timestamp = data.list[i].dt;
+        const date = new Date(timestamp * 1000);
+        const formattedDate = `(${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()})`;
+
+        // Other data about the day
         var temperature = 'Temperature: ' + data.list[i].main.temp + '°F';
         var wind = 'Wind Speed: ' + data.list[i].wind.speed + ' MPH';
         var humid = 'Humidity: ' + data.list[i].main.humidity;
+        // If checks what day it is
+        if (i === 0){
+            var locationEl = document.createElement('span');
+            locationEl.classList = 'header-locationDate';
+            locationEl.textContent = searchTerm + formattedDate;
 
-        console.log(temperature);
-        console.log(wind);
-        console.log(humid);
+            var tempEl = document.createElement('span');
+            tempEl.classList = 'header-temp';
+            tempEl.textContent = temperature;
+
+            var windEl = document.createElement('span');
+            windEl.classList = 'header-wind';
+            windEl.textContent = wind;
+
+            var humidEl = document.createElement('span');
+            humidEl.classList = 'header-humid';
+            humidEl.textContent = humid;
+
+            currentDayContainer.appendChild(locationEl);
+            currentDayContainer.appendChild(tempEl);
+            currentDayContainer.appendChild(windEl);
+            currentDayContainer.appendChild(humidEl);
+        } else {
+            var fiveDayCard = document.createElement('div')
+            fiveDayCard.classList = 'fiveDay-card';
+            
+            var dateEl = document.createElement('span');
+            dateEl.classList = 'header-date';
+            dateEl.textContent = formattedDate;
+
+            var tempEl = document.createElement('span');
+            tempEl.classList = 'header-temp';
+            tempEl.textContent = temperature;
+
+            var windEl = document.createElement('span');
+            windEl.classList = 'header-wind';
+            windEl.textContent = wind;
+
+            var humidEl = document.createElement('span');
+            humidEl.classList = 'header-humid';
+            humidEl.textContent = humid;
+
+            fiveDayCard.appendChild(dateEl);
+            fiveDayCard.appendChild(tempEl);
+            fiveDayCard.appendChild(windEl);
+            fiveDayCard.appendChild(humidEl);
+            fiveDayContainer.appendChild(fiveDayCard);
+        };
     };
+};
+// Does or doesnt add the city to local storage
+var addStorage = function(searchTerm) {
+    console.log('Checking local storage');
+    if(localStorage.getItem('searchTerms')) {
+      searchTerms = JSON.parse(localStorage.getItem('searchTerms'));
+    }
+
+    // Convert term to lowercase before checking if it exists
+    searchTerm = searchTerm.toLowerCase();
+
+    if(!searchTerms.includes(searchTerm)){
+      searchTerms.push(searchTerm);
+      localStorage.setItem('searchTerms', JSON.stringify(searchTerms));
+      addButton(searchTerm);
+    }
+}
+// Adds the past search buttons
+var addButton = function(searchTerm) {
+    var pastSearchEl = document.createElement('button');
+    pastSearchEl.classList = 'pastSearch';
+    pastSearchEl.textContent = searchTerm;
+    pastSearchContainer.appendChild(pastSearchEl);
+};
+// Loads local Storage when page loads or refresh
+function displaySearchTerms() {
+    if(localStorage.getItem('searchTerms')) {
+      searchTerms = JSON.parse(localStorage.getItem('searchTerms'));
+    }
+  
+    for(let i = 0; i < searchTerms.length; i++) {
+      addButton(searchTerms[i]);
+    }
 };
 
 
 
+// First thing to happen
+displaySearchTerms()
+
+// Event listener for search button
 searchButton.addEventListener('click', inputHandler);
+// Event listener for past searches
+document.addEventListener('click', function(event) {
+    if(event.target.classList.contains('pastSearch')) {
+        console.log(event.target.textContent + ' button clicked');
+        document.getElementById('userInput').value = event.target.textContent;
+        getApi( event.target.textContent)
+    }
+});
